1fbce09b7c15b53d560ca7a65f2b02e0a5495ee7
usaxillioner <usaxillioner@gmail.com>
2021-06-23 16:50:45 +0300

    Создал класс Work с методами, (Print, Pause).

---
+++ Lesson1_HW4/Work.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson1_HW4
+{
+    public class Work
+    {
+        public void Print(string message, int coursorPositionX, int coursorPositionY)
+        {
+            Console.SetCursorPosition(coursorPositionX, coursorPositionY);
+            Console.WriteLine(message);
+        }
+        public void Pause()
+        {
+            Console.WriteLine("\n\nПауза.\nДля продолжения нажми любую кнопку.");
+            Console.ReadKey();
+        }
+    }
+}
