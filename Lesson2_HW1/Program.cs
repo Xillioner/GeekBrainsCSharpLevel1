eaf0751a2e75ab9a002110b5cc25503030fc3e2b
usaxillioner <usaxillioner@gmail.com>
2021-06-23 19:43:50 +0300

    Создал проект второго занятия

---
+++ Lesson2_HW1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson2_HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
