48b6b307789a7377e01e8400b07a6e2ca7284158
usaxillioner <usaxillioner@gmail.com>
2021-06-26 23:14:46 +0300

    сделал подсчёт элементов в числе

--- Lesson2_HW2/Program.cs
+++ Lesson2_HW2/Program.cs
@@ -2,11 +2,15 @@
 
 namespace Lesson2_HW2
 {
+    /// <summary>
+    /// Написать метод подсчета количества цифр числа;
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             Work work = new();
+            work.ElementCounter(123048);
         }
     }
 }
--- Lesson2_HW2/Work.cs
+++ Lesson2_HW2/Work.cs
@@ -1,6 +1,14 @@
-﻿namespace Lesson2_HW2
+﻿using System;
+
+namespace Lesson2_HW2
 {
     internal class Work
     {
+        public int NumberLength { get; private set; }
+
+        public void ElementCounter(int number)
+        {
+            NumberLength= number.ToString().Length;
+        }
     }
 }
