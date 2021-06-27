78786c24ffc13227b5a48003e2e3df7ccb46cb6f
usaxillioner <usaxillioner@gmail.com>
2021-06-27 17:10:00 +0300

    Реализовал метод проверки логина и пароля. Метод авторизованной области

---
+++ Lesson2_HW4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Lesson2_HW4
+{
+    /// <summary>
+    /// Реализовать метод проверки логина и пароля. 
+    /// На вход подается логин и пароль. 
+    /// На выходе истина, если прошел авторизацию, и ложь, если не прошел. 
+    /// Используя метод проверки логина и пароля, написать программу: 
+    /// пользователь вводит логин и пароль, программа пропускает его дальше или не пропускает. 
+    /// С помощью цикла do while ограничить ввод пароля тремя попытками; 
+    /// а) Написать программу, которая запрашивает массу и рост человека, 
+    /// вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, 
+    /// набрать вес или все в норме; 
+    /// б) *Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.
+    /// </summary>
+    class Program
+    {
+        private static Work work;
+        static void Main(string[] args)
+        {
+            work = new();
+            int count = 3;
+            string userLogin = null;
+            string userPassword = null;
+            work.Print(new int[] { 40, 0 });
+            if (Convert.ToInt32(Console.ReadLine()) == 0)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                while (count > 0)
+                {
+                    if (!work.IsAuthorized)
+                    {
+                        work.Print(new int[] { 40, 0 }, new int[] { 5 });
+                        userLogin = Console.ReadLine();
+                        work.Print(new int[] { 40, 0 }, new int[] { 6 });
+                        userPassword = Console.ReadLine();
+                        work.Authorize(userLogin, userPassword);
+                    }
+                    count--;
+                }
+                
+                if (work.IsAuthorized) 
+                {
+                    Authorized();
+                    work.AuthorizedArea();
+                }
+                else 
+                { 
+                    work.Print(new int[] { 0, 2 }, new int[] { 4 }); 
+                }
+            }
+        }
+        private static void Authorized()
+        {
+            work.Print(new int[] { 40, 0 }, new int[] { 3 });
+            work.Print(new int[] { 40, 0 },new int[] { 3, 2 });
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lesson2_HW4/Work.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+
+namespace Lesson2_HW4
+{
+    public class Work
+    {
+        const string USERLOGIN = "Xillioner";
+        const string USERPASSWORD = "12345";
+        public bool IsAuthorized { get; private set; }
+        private readonly string[] stringSource=new string[9] { 
+        "Вы не авторизованны, завершение работы приложения!",
+        "0 - Выход",
+        "1 - Продолжить",
+        "Вы авторизованны",
+        "Расчёт индекса массы тела",
+        "Введи логин",
+        "Введи пароль",
+        "1- Расчёт BMI",
+        "Нет такого меню",
+        };
+
+        public void Authorize(string userLogin, string userPassword)
+        {
+            IsAuthorized = USERLOGIN == userLogin && USERPASSWORD == userPassword ? true : false;
+        }
+
+        public void Print(int[] whereToPrint, int[] stringSource = null, string message = null)
+        {
+            Console.Clear();
+            Console.OutputEncoding = Encoding.UTF8;
+            
+
+            if (whereToPrint.Length==2)
+            Console.SetCursorPosition(whereToPrint[0], whereToPrint[1]);
+            Console.WriteLine(this.stringSource[4]);
+            Console.WriteLine();
+
+            if (stringSource!?.Length!=null)
+            {
+                for (int i = 0; i < stringSource.Length; i++)
+                {
+                    Console.WriteLine(this.stringSource[ stringSource[i]]);
+                }
+                
+            }
+            else if (message!=null)
+            {
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine($"\n {this.stringSource[1]}\n {this.stringSource[2]}\n");
+            }
+
+            
+        }
+
+        public void AuthorizedArea()
+        {
+            if (IsAuthorized)
+            {
+                Print(new int[] { 40, 0 }, new int[] { 7 });
+                var userInput=Convert.ToInt32(Console.ReadLine());
+                switch (userInput)
+                {
+                    case 1:
+
+                        break;
+                    default:
+                        Print(new int[] { 40, 1 }, new int[8]);
+                        break;
+                }
+            }
+        }
+    }
+}
