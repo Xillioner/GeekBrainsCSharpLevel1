5802e9255c825e18829dfbd7a6e22e3ea503a42b
usaxillioner <usaxillioner@gmail.com>
2021-06-28 01:12:19 +0300

    BMI формула Расчёт количества кг для снижения веса

--- Lesson2_HW4/Program.cs
+++ Lesson2_HW4/Program.cs
@@ -14,49 +14,65 @@ namespace Lesson2_HW4
     /// набрать вес или все в норме; 
     /// б) *Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.
     /// </summary>
-    class Program
+    class Programs  
     {
         private static Work work;
         static void Main(string[] args)
         {
             work = new();
-            int count = 3;
+            
             string userLogin = null;
             string userPassword = null;
             work.Print(new int[] { 40, 0 });
-            if (Convert.ToInt32(Console.ReadLine()) == 0)
+            var menuStep = 0;
+            do
             {
-                Environment.Exit(0);
-            }
-            else
-            {
-                while (count > 0)
+                work.Print(new int[] { 40, 1 });
+                menuStep = Convert.ToInt32(Console.ReadLine());
+                switch (menuStep)
                 {
-                    if (!work.IsAuthorized)
-                    {
-                        work.Print(new int[] { 40, 0 }, new int[] { 5 });
-                        userLogin = Console.ReadLine();
-                        work.Print(new int[] { 40, 0 }, new int[] { 6 });
-                        userPassword = Console.ReadLine();
-                        work.Authorize(userLogin, userPassword);
-                    }
-                    count--;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        int count = 3;
+                        while (count > 0)
+                        {
+                            if (!work.IsAuthorized)
+                            {
+                                work.Print(new int[] { 40, 0 }, new int[] { 5 });
+                                userLogin = Console.ReadLine();
+                                work.Print(new int[] { 40, 0 }, new int[] { 6 });
+                                userPassword = Console.ReadLine();
+                                work.Authorize(userLogin, userPassword);
+                            }
+                            if (!work.IsAuthorized)
+                                count--;
+                            else break;
+                        }
+
+                        if (work.IsAuthorized)
+                        {
+                            Authorized();
+                            work.AuthorizedArea();
+                            string extraWeight = work.extraWeight == 0 ? "" : $"{work.extraWeight:F1} Кг";
+                            work.Print(new int[] { 40, 1 }, null, $"{work.IndexBodyWeight} - {work.IndexHelper} {extraWeight}\n1 - Продолжить");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            work.Print(new int[] { 0, 2 }, new int[] { 4 });
+                        }
+                        break;
+                    default:
+                        work.Print(new int[] { 40, 1 });
+                        break;
                 }
-                
-                if (work.IsAuthorized) 
-                {
-                    Authorized();
-                    work.AuthorizedArea();
-                }
-                else 
-                { 
-                    work.Print(new int[] { 0, 2 }, new int[] { 4 }); 
-                }
-            }
+            } while (menuStep != 0);
         }
         private static void Authorized()
         {
-            work.Print(new int[] { 40, 0 }, new int[] { 3 });
+            //work.Print(new int[] { 40, 0 }, new int[] { 3 });
             work.Print(new int[] { 40, 0 },new int[] { 3, 2 });
             Console.ReadKey();
 
--- Lesson2_HW4/Work.cs
+++ Lesson2_HW4/Work.cs
@@ -8,7 +8,26 @@ namespace Lesson2_HW4
         const string USERLOGIN = "Xillioner";
         const string USERPASSWORD = "12345";
         public bool IsAuthorized { get; private set; }
-        private readonly string[] stringSource=new string[9] { 
+        public double? IndexBodyWeight
+        {
+            get
+            {
+                if (IsAuthorized)
+                {
+                    return indexBodyWeight;
+
+                }
+                return null;
+            }
+            private set { }
+        }
+
+        private double indexBodyWeight;
+        public double extraWeight { get; private set; }
+
+        public string IndexHelper { get { return IndexHelperInquire(); } private set { } }
+
+        private readonly string[] stringSource = new string[21] {
         "Вы не авторизованны, завершение работы приложения!",
         "0 - Выход",
         "1 - Продолжить",
@@ -16,8 +35,20 @@ namespace Lesson2_HW4
         "Расчёт индекса массы тела",
         "Введи логин",
         "Введи пароль",
-        "1- Расчёт BMI",
+        "1 - Расчёт BMI",
         "Нет такого меню",
+        "Введи свой вес (кг)",
+        "Введи свой рост (см)",
+        "Выраженный дефицит массы тела",
+        "Недостаточная (дефицит) массы тела",
+        "Норма",
+        "Избыточная масса тела (предожирение)",
+        "Ожирение 1 степени",
+        "Ожирение 2 степени",
+        "Ожирение 3 степени",
+        " - вам следует набрать",
+        " - вам следует похудеть на",
+        "1 - Логин",
         };
 
         public void Authorize(string userLogin, string userPassword)
@@ -29,31 +60,31 @@ namespace Lesson2_HW4
         {
             Console.Clear();
             Console.OutputEncoding = Encoding.UTF8;
-            
 
-            if (whereToPrint.Length==2)
-            Console.SetCursorPosition(whereToPrint[0], whereToPrint[1]);
+
+            if (whereToPrint.Length == 2)
+                Console.SetCursorPosition(whereToPrint[0], whereToPrint[1]);
             Console.WriteLine(this.stringSource[4]);
             Console.WriteLine();
 
-            if (stringSource!?.Length!=null)
+            if (stringSource!?.Length != null)
             {
                 for (int i = 0; i < stringSource.Length; i++)
                 {
-                    Console.WriteLine(this.stringSource[ stringSource[i]]);
+                    Console.WriteLine(this.stringSource[stringSource[i]]);
                 }
-                
+
             }
-            else if (message!=null)
+            else if (message != null)
             {
                 Console.WriteLine(message);
             }
             else
             {
-                Console.WriteLine($"\n {this.stringSource[1]}\n {this.stringSource[2]}\n");
+                Console.WriteLine($"\n {this.stringSource[1]}\n {this.stringSource[20]}\n");
             }
 
-            
+
         }
 
         public void AuthorizedArea()
@@ -61,16 +92,76 @@ namespace Lesson2_HW4
             if (IsAuthorized)
             {
                 Print(new int[] { 40, 0 }, new int[] { 7 });
-                var userInput=Convert.ToInt32(Console.ReadLine());
+                var userInput = Convert.ToInt32(Console.ReadLine());
                 switch (userInput)
                 {
                     case 1:
-
+                        Print(new int[] { 40, 1 }, new int[] { 9 });
+                        var weight = Convert.ToDouble(Console.ReadLine());
+                        Print(new int[] { 40, 1 }, new int[] { 10 });
+                        var height = Convert.ToDouble(Console.ReadLine()) / 100;
+                        Bmi(weight, height);
+                        ExtraWeight(height);
                         break;
                     default:
                         Print(new int[] { 40, 1 }, new int[8]);
                         break;
                 }
+            }
+        }
+
+        private void ExtraWeight(double height)
+        {
+            double normalIndexMin = 18.5;
+            double normalIndexMax = 25;
+            double extraIndex;
+
+            if (IsAuthorized && indexBodyWeight > normalIndexMax)
+            {
+                 extraIndex = indexBodyWeight - normalIndexMax;
+                extraWeight = extraIndex * Math.Pow(height, 2);
+            }
+            else if(IsAuthorized&&indexBodyWeight<normalIndexMin)
+            {
+                extraIndex = normalIndexMin-indexBodyWeight;
+                extraWeight = extraIndex * Math.Pow(height, 2);
+            }
+        }
+
+        private void Bmi(double weight, double height)
+        {
+            if (IsAuthorized)
+                indexBodyWeight = weight / Math.Pow(height, 2);
+        }
+        private string IndexHelperInquire()
+        {
+            if (IsAuthorized && indexBodyWeight < 17)
+            {
+                return stringSource[11] + stringSource[18];
+            }
+            else if (indexBodyWeight < 18.5)
+            {
+                return stringSource[12] + stringSource[18];
+            }
+            else if (indexBodyWeight < 25)
+            {
+                return stringSource[13];
+            }
+            else if (indexBodyWeight < 30)
+            {
+                return stringSource[14] + stringSource[19];
+            }
+            else if (indexBodyWeight < 35)
+            {
+                return stringSource[15] + stringSource[19];
+            }
+            else if (indexBodyWeight < 40)
+            {
+                return stringSource[16] + stringSource[19];
+            }
+            else
+            {
+                return stringSource[17] + stringSource[19];
             }
         }
     }
